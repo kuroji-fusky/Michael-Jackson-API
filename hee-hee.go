639f00a0468492df7259aa2f56e4ab02f829b4be
Kerby Keith Aquino <skepfoosky15@gmail.com>
2022-11-04 11:18:27 -0400

    Added actual quotes this time HEE HEE

--- hee-hee.go
+++ hee-hee.go
@@ -6,10 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Quotes struct {
+	quote  string
+	source string
+}
+
 var quotes = []string{
-	"HEE HEE",
-	"SHAMONE",
-	"AOOW",
+	"All of us are products of our childhood.",
+	"To give someone a piece of your heart, is worth more than all the wealth in the world.",
+	"A star can never die. It just turns into a smile and melts back into the cosmic music, the dance of life.",
+	"I'm just like anyone. I cut and I bleed and I embarass easily.",
+	"Look beyond yourself...",
+	"Success, fame, and fortune, they're all illusions. All there is that is real is the friendship that two can share.",
 }
 
 func AllMJQuotes(c *gin.Context) {
@@ -21,5 +29,5 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", AllMJQuotes)
-	router.Run("localhost:6969")
+	router.Run("localhost:8000")
 }
